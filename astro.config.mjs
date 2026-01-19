41d7ef1999663b721192c90caa46d74a8835ec4b
tapabnamfarm-cmyk <tapabnam.farm@gmail.com>
2026-01-19 19:30:32 +0700

    fix astro netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
-  site: "https://tapabnamfarm.com"
-});
+  site: "https://tapabnamfarm.netlify.app",
+    output: "static",
+      adapter: netlify(),
+      });
